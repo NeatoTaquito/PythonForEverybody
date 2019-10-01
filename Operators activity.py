1d82c2a0a60e10bc45e991d0f81de1f1e597cd93
NeatoTaquito <54412487+NeatoTaquito@users.noreply.github.com>
2019-10-01 18:47:37 -0400

    Changing strings to int
    Good practice for conditional operator changing.

---
+++ Operators activity.py
@@ -0,0 +1,6 @@
+firstNumber = input("Enter a number: ")
+secondNumber = input("Enter a second number: ")
+num1 = int(firstNumber)
+num2 = int(secondNumber)
+difference = num1 - num2
+print("Difference = ", difference)
