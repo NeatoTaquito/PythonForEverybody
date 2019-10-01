62bc69dfec5d6d4cb55469139a80871d623654d2
NeatoTaquito <54412487+NeatoTaquito@users.noreply.github.com>
2019-10-01 18:38:45 -0400

    COP 1500 hardest exercise review.
    Made this in order to understand how exactly to do so in the future when using Booleans in such a specific way to find ONLY evens or odds.

---
+++ Exercise Practice.py
@@ -0,0 +1,9 @@
+# Use "for" loop to show range 1 to 21
+
+# Use Modulus to check that the result it NOT equal to 0
+
+# Print odds
+
+for i in range(1,21):
+    if ((i % 2) != 0):
+        print("i = ", i)
