f883f6e3852da3cbe596e4fe71d5f9574913abe5
NeatoTaquito <54412487+NeatoTaquito@users.noreply.github.com>
2019-10-29 11:59:38 -0400

    Add files via upload

---
+++ Python Exercise 6 Practice.py
@@ -0,0 +1,10 @@
+def is_leap(year):
+    leap = False
+    if leap / 4 and leap!= 0:
+        if leap / 100 and leap != 0:
+            break
+            if leap / 400 and leap !=0:
+                leap = True
+        return leap
+year = int(input())
+print(is_leap(year))
